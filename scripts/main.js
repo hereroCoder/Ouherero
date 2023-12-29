cd6105ef54d1597a2c3f361c327ff60d4dcbc200
hereroCoder <83869107+hereroCoder@users.noreply.github.com>
2023-12-29 14:17:30 +0000

    universal section styles and navigation to each section

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,5 @@
-// const myButton = document.getElementById('nextImg');
-// const myImage = document.querySelector('img');
-// const userButton =  document.getElementById('userBtn');
-// const greetText = document.querySelector('h1');
 
-
-// myButton.onclick = () =>{
-//   const theSource = myImage.getAttribute('src')
-
-//   if (theSource === '/images/cattle.jpeg'){
-//     myImage.setAttribute('src','/images/chickens.jpeg')
-//   }
-//   else{
-//     myImage.setAttribute('src','/images/cattle.jpeg')
-//   }
-// };
-
-// userButton.onclick = ()=>{
-//   const name = prompt("What is your name?");
-//   greetText.textContent = `How are you doing ${name}`;
-// }
-
+// toggle menu
 function openMenu (){
   document.querySelector('nav').style.right = "0";
 }
